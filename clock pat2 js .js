c62fd0a754f565a85c90bfa76ee287432afce021
joylesspartho <51858206+joylesspartho@users.noreply.github.com>
2024-02-17 01:56:51 +0600

    Add files via upload

---
+++ clock pat2 js .js
@@ -0,0 +1,21 @@
+function clock_tim() {
+    let hr = document.getElementById('hour');
+    let min = document.getElementById('minet');
+    let sec = document.getElementById('secend');
+
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hRotation = 30 * hh + mm / 2;
+    let mRotation = 6 * mm;
+    let sRotation = 6 * ss;
+
+   
+    hr.style.transform = `rotate(${hRotation}deg)`;
+    min.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+}
+
+setInterval(clock_tim, 1000);
